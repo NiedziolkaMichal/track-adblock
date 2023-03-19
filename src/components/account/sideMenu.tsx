30756604f02ad16a605ade961f0d209f785ddbec
Niedziolka Michal <niedziolek@hotmail.com>
2023-03-19 22:28:00 +0100

    Add `installation` button to user panel

--- src/components/account/sideMenu.tsx
+++ src/components/account/sideMenu.tsx
@@ -11,6 +11,9 @@ export function SideMenu() {
         <MenuItemLink title="Strona główna" href="/account">
           <IconIndex />
         </MenuItemLink>
+        <MenuItemLink title="Instalacja" href="/account/addHost" additionalPaths={["/account/install/analytics"]}>
+          <IconInstallation />
+        </MenuItemLink>
         <MenuItemLink title="Płatności" href="/account/payments">
           <IconPayment />
         </MenuItemLink>
@@ -115,9 +118,9 @@ const ItemTitle = styled.div`
   overflow: hidden; //TODO handle it better way
 `;
 
-function MenuItemLink({ title, href, children }: { title: string; href: string; children: ReactNode }) {
+function MenuItemLink({ title, href, additionalPaths, children }: { title: string; href: string; additionalPaths?: string[]; children: ReactNode }) {
   const router = useRouter();
-  const isActive = router.pathname === href;
+  const isActive = router.pathname === href || (!!additionalPaths && additionalPaths.includes(router.pathname));
   return (
     <Item href={href} $active={isActive}>
       {children}
@@ -142,6 +145,14 @@ function IconIndex() {
   return (
     <ItemIcon fill="currentColor" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
       <path d="M0 25.406h22.406v-1.75h-20.656v-17.063h-1.75v18.813zM3.063 21.969h19.25v-13.813l-4.063 3.719-3.781-1.375-4 4.563-4.094-1.469-3.313 3.438v4.938z"></path>
+    </ItemIcon>
+  );
+}
+
+function IconInstallation() {
+  return (
+    <ItemIcon fill="currentColor" viewBox="0 0 256 241" xmlns="http://www.w3.org/2000/svg">
+      <path d="M254 188V2H2v186h111v29H75v22h106v-22h-38v-29h111zM19 19h217v151H19V19zm110.23 48.81V60.5l-7.68-1.3a22.27 22.27 0 0 0-2.9-6.98l4.52-6.34-5.18-5.18-6.35 4.51a22.28 22.28 0 0 0-6.97-2.9l-1.3-7.67h-7.33l-1.29 7.68a22.3 22.3 0 0 0-6.98 2.89l-6.34-4.51-5.18 5.18 4.51 6.34a22.27 22.27 0 0 0-2.9 6.98l-7.67 1.3v7.32l7.68 1.3a22.25 22.25 0 0 0 2.89 6.98l-4.51 6.34 5.18 5.18 6.34-4.51A22.18 22.18 0 0 0 94.75 86l1.3 7.68h7.32l1.3-7.68a22.18 22.18 0 0 0 6.97-2.9l6.35 4.52 5.18-5.18-4.51-6.34a22.25 22.25 0 0 0 2.89-6.98l7.68-1.3zM99.7 75.8a11.65 11.65 0 1 1 0-23.3 11.65 11.65 0 0 1 0 23.3zm80.53 34.03 8.95-5.56-3.76-9.17-10.27 2.32a30.1 30.1 0 0 0-7.2-7.26l2.4-10.25-9.13-3.83-5.63 8.9a30.1 30.1 0 0 0-10.22-.05l-5.55-8.95-9.17 3.76L132.97 90a30.1 30.1 0 0 0-7.25 7.2l-10.26-2.4-3.83 9.13 8.9 5.63a30.1 30.1 0 0 0-.04 10.22l-8.95 5.55 3.75 9.17 10.28-2.3a30.1 30.1 0 0 0 7.19 7.25l-2.4 10.25 9.14 3.83 5.63-8.9c3.43.6 6.88.6 10.21.04l5.56 8.95 9.17-3.75-2.31-10.28a30.1 30.1 0 0 0 7.25-7.19l10.26 2.4 3.83-9.14-8.9-5.63c.6-3.43.6-6.88.04-10.21zm-15.35 11.07a15.75 15.75 0 1 1-29.05-12.18 15.75 15.75 0 0 1 29.05 12.18zm-73.14-1.18a5.4 5.4 0 1 1-10.8 0 5.4 5.4 0 0 1 10.8 0zm14.13 2.43v-4.85l-5.08-.86a14.75 14.75 0 0 0-1.91-4.61l2.99-4.2-3.43-3.43-4.2 2.98a14.74 14.74 0 0 0-4.62-1.9l-.86-5.09h-4.85l-.85 5.08a14.76 14.76 0 0 0-4.62 1.91l-4.2-2.98-3.43 3.43 2.99 4.2a14.73 14.73 0 0 0-1.91 4.61l-5.09.86v4.85l5.09.85a14.73 14.73 0 0 0 1.9 4.62l-2.98 4.2 3.43 3.43 4.2-2.99c1.4.89 2.95 1.54 4.62 1.92l.85 5.08h4.85l.86-5.08a14.69 14.69 0 0 0 4.62-1.92l4.2 2.99 3.42-3.43-2.98-4.2a14.6 14.6 0 0 0 1.91-4.62l5.08-.85z" />;
     </ItemIcon>
   );
 }
